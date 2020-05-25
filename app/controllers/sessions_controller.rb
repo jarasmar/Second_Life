b56bb446d12d162a7d301e395be5314f00e02389
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-05-25 11:26:11 +0100

    Added some of the login and logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
 class SessionsController < ApplicationController
   def create
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      sessions[:user_id] = @user.user_id
+      redirect_to '/home'
+    else
+    redirect_to '/login'
+    end
   end
 
   def login
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
-  get 'sessions/login'
-  get 'sessions/welcome'
-  get 'users/new'
-  get 'users/create'
+  resources :users, only: [:new, :create]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  # get 'sessions/create'
+  # get 'sessions/login'
+  # get 'sessions/welcome'
+  # get 'users/new'
+  # get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 end

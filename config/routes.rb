7957e9acf9901e035b4a13fbc279f46b5ba0d1e6
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-05-25 10:42:10 +0100

    created user model, and controllers users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+  get 'sessions/login'
+  get 'sessions/welcome'
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 end

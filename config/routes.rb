63feba39ebfff27e5072006f52f37a4d722ccd1f
Jara Santamaria <jarasmar@gmail.com>
2020-05-26 12:29:24 +0100

    add routes redirecting to login when no current user

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
   delete 'logout', to: 'sessions#destroy'
-  # get 'sessions/create'
-  # get 'sessions/login'
-  # get 'sessions/welcome'
-  # get 'users/new'
-  # get 'users/create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root 'home#index'
 end

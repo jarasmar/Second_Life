91f817927dee36a2ad476c5c05fe1b2546c7d2e5
Jara Santamaria <jarasmar@gmail.com>
2020-06-10 15:05:53 +0100

    add email format and uniqueness validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+  
   validates_uniqueness_of :username, :message => '%{value} has already been taken'
+  validates_uniqueness_of :email, :message => '%{value} has already been taken'
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  
   has_many :posts
 end

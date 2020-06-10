50d591456fd5d4d9dbdfc7bf94425b59612a452d
Jara Santamaria <jarasmar@gmail.com>
2020-06-10 15:47:31 +0100

    add password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   
   validates_uniqueness_of :username, :message => '%{value} has already been taken'
   validates_uniqueness_of :email, :message => '%{value} has already been taken'
-  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  
+  validates :password, length: { in: 5..20 }
   
   has_many :posts
+
 end

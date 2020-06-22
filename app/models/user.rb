392c9ea23c926c4976803e4c9eb4d44ec07662ac
Jara Santamaria <jarasmar@gmail.com>
2020-06-22 15:34:34 +0100

    Add validation for username presence at signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
   has_one_attached :profile_image
+
+  validates :username, :email, :password, presence: true
   
   validates_uniqueness_of :username, :message => '%{value} has already been taken'
   validates_uniqueness_of :email, :message => '%{value} has already been taken'
   validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
-  
   validates :password, length: { in: 5..20 }
   
   has_many :posts

2e38346941b16e015382daa0061f30ce1e13689e
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-06-25 13:21:22 +0100

    WIP with working with json and API

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,14 @@ class PostsController < ApplicationController
     @user = User.find(current_user.id)
     @post = Post.create(post_params)
     @post.user_id = @user.id
+    response = ('https://maps.googleapis.com/maps/api/geocode/json', {
+      params: {
+        address: params[:location],
+        key: '<%= Rails.application.credentials.dig(:google, :google_api_key) %>'
+      }
+    })
+    @post.lat = response.data.results[0].geometry.location.lat
+    @post.lng = response.data.results[0].geometry.location.lng
 
     if @post.save
       redirect_to '/'
@@ -49,6 +57,6 @@ class PostsController < ApplicationController
   private
   def post_params
     user_id = @user.id
-    params.require(:post).permit(:id, :main_image, :title, :description, :user_id)
+    params.require(:post).permit(:id, :main_image, :title, :description, :user_id, :location)
   end
 end

6f8c91aa5de5e2d8620f7a382ad36e8f412cc3ec
James Oddy <jamesoddy8@gmail.com>
2020-07-22 10:31:33 +0100

    deleted dark css stylesheet and removed line from post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -32,5 +32,4 @@ class Post < ApplicationRecord
       self.main_image.attach(io: File.open(Rails.root.join("app", "assets", "images", "default_main_image.png")), filename: 'default_main_image.png' , content_type: "image/png")
     end
   end
-
 end

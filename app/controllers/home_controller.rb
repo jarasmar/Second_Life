2fe2beecbb55add5fb877d6fa9ff80c87a23952b
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-08-06 12:24:19 +0100

    made an intereative map that displays all item markers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
+  
   def index
     @posts = Post.all
   end
+
+  def map
+    @posts = Post.all
+    @api_key = Rails.application.credentials.dig(:google, :google_api_key)
+    gon.posts = Post.all
+  end
+
 end

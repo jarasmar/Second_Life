26b5a7ed0e280d3b5c2fb450bb044da7332dfc4e
jarasmar <jarasmar@gmail.com>
2020-09-23 14:10:54 +0200

    Fix redirections for logged in users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :require_login
-  before_action :redirect_if_logged_in
+  before_action :redirect_if_logged_in, only: [:new, :create]
   
   def create
     @user = User.find_by(username: params[:username])
